c4f1a636b27c7fa0a7be9884ff250ad26c0de277
RenatoVA <renato.vivas2001@gmail.com>
2024-11-14 10:55:54 -0500

    some changes x6

--- functions/main.py
+++ functions/main.py
@@ -1,4 +1,4 @@
 import sys
-sys.path.append("..")  # Permite que el archivo de funciones acceda a archivos externos
+sys.path.append(".")  # Permite que el archivo de funciones acceda a archivos externos
 
 from app.main import handler
