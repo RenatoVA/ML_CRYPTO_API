3fceb6d3b7005e97908d9510edc08be3ba7e6ca4
RenatoVA <renato.vivas2001@gmail.com>
2024-11-14 11:37:59 -0500

    vercel

--- app/main.py
+++ app/main.py
@@ -62,6 +62,3 @@ def get_top_5_tokens():
         return top_5_per_category
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
